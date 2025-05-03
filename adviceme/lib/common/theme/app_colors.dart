76c6638eeabe6b1bdd6984cf389b1f8fd73c05f4
Milena Tenorio <milena.rtenorio@gmail.com>
2025-05-03 11:49:12 -0400

    Add icon app

--- adviceme/lib/common/theme/app_colors.dart
+++ adviceme/lib/common/theme/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
 
   static const yellowBackground = Color(0xFFFFFFB5);
   static const blueCard = Color(0xFFABDEE6);
-  static const purpleCard = Color.fromARGB(255, 250, 226, 250);
+  static const purpleCard = Color(0xFFFAE2FA);
   static const pinkBackground = Color(0xFFF3B0C3);
   static const orangeBackground = Color(0xFFFFCCB6);
   static const redHeart = Color.fromARGB(255, 220, 133, 156);
